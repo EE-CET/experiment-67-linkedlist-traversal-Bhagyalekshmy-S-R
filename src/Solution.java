f0fe59e1706d4a0c6d6ec1604639023cfa905267
Your Name <you@example.com>
2026-02-27 23:42:42 +0530

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -6,18 +6,34 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // TODO: Read the integer N
+        // Read the integer N
+        int N = scanner.nextInt();
         
-        // TODO: Create a LinkedList of Integers
+        // Create a LinkedList of Integers
+        LinkedList<Integer> list = new LinkedList<>();
         
-        // TODO: Read N integers and add them to the LinkedList
+        // Read N integers and add them to the LinkedList
+        for (int i = 0; i < N; i++) {
+            list.add(scanner.nextInt());
+        }
         
-        // TODO: Create a ListIterator for the LinkedList
+        // Create a ListIterator for the LinkedList
+        ListIterator<Integer> iterator = list.listIterator();
         
-        // TODO: Traverse the list in the forward direction and print the elements
+        // Traverse forward
+        System.out.print("Forward: ");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
         
-        // TODO: Traverse the list in the backward direction and print the elements
+        System.out.println();
         
+        // Traverse backward
+        System.out.print("Backward: ");
+        while (iterator.hasPrevious()) {
+            System.out.print(iterator.previous() + " ");
+        }
         
+        scanner.close();
     }
 }
